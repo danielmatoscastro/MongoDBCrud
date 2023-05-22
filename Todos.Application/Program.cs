0fa50f06d00e46c026100e5e703f9684f5e77f6a
Daniel Castro <daniel.matos.castro@gmail.com>
2023-05-22 20:47:20 -0300

    mend

--- Todos.Application/Program.cs
+++ Todos.Application/Program.cs
@@ -1,6 +1,10 @@
+using Todos.Infra.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDBOptions>(builder.Configuration.GetSection(MongoDBOptions.MongoDB));
+builder.Services.AddSingleton<MongoDBDataAccess>();
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();

523108afdd0d23921018446483fd638257bc6afb
Daniel Castro <daniel.matos.castro@gmail.com>
2023-08-19 16:08:36 -0300

    add endpoint to toogle complete status

--- Todos.Application/Controllers/TodoListsController.cs
+++ Todos.Application/Controllers/TodoListsController.cs
@@ -105,4 +105,27 @@ public class TodoListsController : ControllerBase
 
         return Ok(todoEntity.ToReadTodoDTO());
     }
+
+    [HttpPost("{todoListId}/items/{todoId}")]
+    public async Task<IActionResult> ToogleCompletedTodo(string todoListId, string todoId)
+    {
+        var todoListEntity = await _todoListRepository.GetById(todoListId);
+        if (todoListEntity == null)
+        {
+            return NotFound();
+        }
+
+        var todoEntity = todoListEntity.Items.FirstOrDefault(item => item.Id == todoId);
+        if (todoEntity == null)
+        {
+            return NotFound();
+        }
+
+        todoEntity.CompletedAt = todoEntity.Completed ? null : DateTime.UtcNow;
+        todoEntity.Completed = !todoEntity.Completed;
+
+        await _todoListRepository.Update(todoListEntity);
+
+        return Ok(todoEntity.ToReadTodoDTO());
+    }
 }
